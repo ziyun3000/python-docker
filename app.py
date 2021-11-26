4a5f6b070287954f8ab002c1a731087fca51065d
Wende Song <ziyun3000@126.com>
2021-11-26 08:07:36 +0000

    Test workflow 4

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "Let's test workflow again again!"
+  return "Let's test workflow again again again!"
 
 @app.route('/widgets')
 def get_widgets():
