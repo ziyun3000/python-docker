3f1531569f632d27532dd7eed5a8a9efc9122f7c
Wende Song <ziyun3000@126.com>
2021-11-26 07:55:07 +0000

    Test workflow 3
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "Let's test workflow again!"
+  return "Let's test workflow again again!"
 
 @app.route('/widgets')
 def get_widgets():
